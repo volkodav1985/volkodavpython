2f1c81525ec205c22c54f3153c80ff38a1072fa7
volkodav1985 <volkodav_85@list.ru>
2016-07-05 23:57:21 -0400

    1 неделя. Задание 3 (контакты)

---
+++ contact.py
@@ -0,0 +1,10 @@
+class Contact:
+    def __init__(self, name, surname, job, mainaddress, phone, year, secondaddress):
+        self.name=name
+        self.surname=surname
+        self.job=job
+        self.mainaddress=mainaddress
+        self.phone=phone
+        self.year=year
+        self.secondaddress=secondaddress
+
