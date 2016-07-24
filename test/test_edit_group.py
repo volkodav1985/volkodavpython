9b9488c1caadf6b5df51e2abb9d32c1c4f8a6f97
volkodav1985 <volkodav_85@list.ru>
2016-07-23 21:26:39 -0400

    Задание №11: Реализованы проверки в виде сравнения списков

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,9 @@
 from model.group import Group
 
 def test_edit_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.edit(Group(name="aaaaa", header="kkk", footer="ccc"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,13 +5,19 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="Loli"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 
 def test_modify_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(header="test"))
     app.group.modify_first_group(Group(header="Guchi"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
